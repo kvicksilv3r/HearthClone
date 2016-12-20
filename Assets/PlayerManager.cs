d7ff9b15cebf6d471f5691bf4991fa5082ad91fd
TacoBellDoge <aforandreas@hotmail.com>
2016-12-20 13:36:00 +0100

    Hero stats and actions
    Hero bilderna med canvas för hp och knapp för ability

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Text playerTextHP;
+    int playerHP;
+    public int playerMaxHP = 30;
+
+	void Start ()
+    {
+        playerHP = playerMaxHP;
+        playerTextHP.text = playerHP.ToString();
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void PlayerAbility()
+    {
+        //if(priest)
+        //{
+        //  PriestAbility();
+        //}
+
+        //if(Warlock)
+        //{
+        //  WarlockAbility();
+        //}
+    }
+
+    void WarlockAbility()
+    {
+        playerHP -= 2;
+        //DrawCard();
+    }
+    
+    void PriestAbility()
+    {
+        //point to object and heal object with 2
+    }
+}

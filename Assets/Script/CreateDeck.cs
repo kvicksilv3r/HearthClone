f60807e87831681c2ade54de0e1e19d7b809b79c
AragonKommer <klomum142@hotmail.com>
2016-12-22 23:44:41 +0100

    Add files via upload

---
+++ Assets/Script/CreateDeck.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+/* TO DO LIST:
+ * Vet inte hur vi gör när spelaren ska välja ett kort.
+ * Det som behövs är meny när man startar med en ny kortlek eller om man använda en sparad lek. 
+ * Det som finns nu är att man ändrar "Selected_Card" och sedan klickar på en W eller A för att lägga till eller ta bort kortet.
+ * Man kan få en ny lek eller en sparad lek.
+ * */
+
+public class CreateDeck : MonoBehaviour
+{
+    public int[] Players_Deck; //Players card deck
+    public int Selected_Card = 001;
+    void Start()
+    {
+
+    }
+    void LoadNewDeck()
+    {
+        Players_Deck = new int[] {001, 002, 003, 005, 006, 007, 008, 009, 010 };// Player gets a new deck
+    }
+    void LoadSavedDeck()
+    {
+        gameObject.GetComponent<SaveFile>().read();
+        Players_Deck = gameObject.GetComponent<SaveFile>().Load_cards;
+
+    }
+
+    void addCard(int cardID) //adds card to Players card deck
+    {
+        List<int> cardList = new List<int>(Players_Deck);
+        Debug.Log("ADD CARD");
+        int countItem = cardList.Where(x => x.Equals(cardID)).Count(); // Counting if there are any duplicates of the selected card in Players_deck
+        if (countItem < 2)
+        {
+            cardList.Add(cardID);
+            Players_Deck = cardList.ToArray();
+        }
+
+    }
+    void removeCard(int cardID) //removes cars from Plaers card deck
+    {
+        List<int> cardList2 = new List<int>(Players_Deck);
+        cardList2.Remove(cardID);
+        Players_Deck = cardList2.ToArray();
+        Debug.Log("Removes card");
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        { 
+            addCard(Selected_Card); // add card to Players_deck
+        }
+        if (Input.GetKeyDown(KeyCode.A)) // change input [temporary input]
+        {
+          
+            removeCard(Selected_Card); // tar bort ett kort från Players card deck
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            LoadSavedDeck();
+        }
+        
+    }
+
+}
--- Assets/Script/SaveFile.cs
+++ Assets/Script/SaveFile.cs
@@ -8,111 +8,102 @@ using System.Linq;
 public class SaveFile : MonoBehaviour
 {
 
-	/*GUI WINDOW*/
-	public Rect windowRect = new Rect(0, 0, Screen.width, Screen.height);
-	private bool showWindow = true;
-	/*saving data*/
-	public string filePath_cards;
-	public string filePath_gameData;
-	/*test string array*/
-	public string[] cards;
-	public string[] gameData;
-	public string[] Load_gameData;
-	public string[] Load_cards;
+    /*GUI WINDOW*/
+    public Rect windowRect = new Rect(0, 0, Screen.width, Screen.height);
+    private bool showWindow = true;
+    /*saving data*/
+    public string filePath_cards;
+    public string filePath_gameData;
+    /*test string array*/
+    public string[] cards;
+    public string[] gameData;
+    public string[] Load_gameData;
+    public int[] Load_cards;
 
 
-	void Start()
-	{
-		filePath_cards = System.IO.Path.Combine(Application.streamingAssetsPath, "SavedCards"); //gets the path for the streamingAssets folder where there is a file called "SavedData".
-		filePath_gameData = System.IO.Path.Combine(Application.streamingAssetsPath, "SavedData");
-		windowRect = new Rect(0, 0, Screen.width, Screen.height); //GUI 
+    void Start()
+    {
+        filePath_cards = System.IO.Path.Combine(Application.streamingAssetsPath, "SavedCards"); //gets the path for the streamingAssets folder where there is a file called "SavedData".
+        filePath_gameData = System.IO.Path.Combine(Application.streamingAssetsPath, "SavedData");
+        windowRect = new Rect(0, 0, Screen.width, Screen.height); //GUI 
 
-	}
-	/*GUI */
-	void OnGUI()
-	{
-		if (showWindow == true)
-		{
-			windowRect = GUI.Window(1, windowRect, DoMyWindow, "Save");
+    }
+    /*GUI  buttons to testing if save and read works*/
+    void OnGUI()
+    {
+        if (showWindow == true)
+        {
+            windowRect = GUI.Window(1, windowRect, DoMyWindow, "Save");
 
-		}
-	}
-	void DoMyWindow(int windowID)
-	{
-		if (windowID == 1)
-		{
-			if (GUI.Button(new Rect(250, 200, 100, 20), "Save"))
-			{
-				showWindow = false;
-				save();
-			}
-			if (GUI.Button(new Rect(250, 200, 100, 20), "Read"))
-			{
-				read();
-			}
-		}
+        }
+    }
+    void DoMyWindow(int windowID)
+    {
+        if (windowID == 1)
+        {
+            if (GUI.Button(new Rect(250, 200, 100, 20), "Save"))
+            {
+                showWindow = false;
+                save();
+            }
+            if (GUI.Button(new Rect(550, 200, 100, 20), "Read"))
+            {
+                read();
+            }
+        }
 
-	}
+    }
 
-	/*For saving data*/
-	void save()
-	{
-		//creates or replace a text file and add every element from stringFile as new line of text.
-		using (StreamWriter sw = new StreamWriter(filePath_cards))
-		{
-			cards = gameObject.GetComponent<StringStorage>().Players_Deck;
-			for (int i = 0; i < cards.GetLongLength(0); i++)
-			{
-				sw.WriteLine(cards[i]);
-			}
+/*For saving data*/
+    void save()
+    {
+        /*creates or replace a text file and add every element from stringFile as new line of text.*/
+         using (StreamWriter sw = new StreamWriter(filePath_cards))
+         {
+             //cards = gameObject.GetComponent<StringStorage>().Players_Deck;
+             for (int i = 0; i < cards.GetLongLength(0); i++)
+             {
+                 sw.WriteLine(cards[i]); 
+             }
 
-		}
-		using (StreamWriter sw = new StreamWriter(filePath_gameData))
-		{
-			//gameData = gameObject.GetComponent<ParseToJSON>().parsed_gameData;
+         }
+         using (StreamWriter sw = new StreamWriter(filePath_gameData))
+         {
+             gameData = gameObject.GetComponent<ParseToJSON>().parsed_gameData;
 
-			for (int i = 0; i < cards.GetLongLength(0); i++)
-			{
-				sw.WriteLine(gameData[i]);
-			}
+             for (int i = 0; i < cards.GetLongLength(0); i++)
+             {
+                 sw.WriteLine(gameData[i]);
+             }
 
-		}
-	}
-	/*reads files*/
-	void read()
-	{
-		using (StreamReader sr = new StreamReader(filePath_cards))
-		{
-			List<string> cardDataList = new List<string>(Load_cards);
-			while (sr.Peek() >= 0)
-			{
-				cardDataList.Add(sr.ReadLine()); //adds one line at a time to datalist 
-			}
-			Load_cards = cardDataList.ToArray(); //converts the list to an string array 
-			for (int i = 0; i < Load_cards.GetLength(0); i++)
-			{
-				Debug.Log("Load_cards:" + Load_cards[i]);
-			}
-		}
-		using (StreamReader sr = new StreamReader(filePath_gameData))
-		{
-			List<string> gameDataList = new List<string>(Load_gameData);
-			while (sr.Peek() >= 0)
-			{
-				gameDataList.Add(sr.ReadLine()); //adds one line at a time to datalist 
-			}
-			Load_gameData = gameDataList.ToArray(); //converts the list to an string array 
-			for (int i = 0; i < Load_gameData.GetLength(0); i++)
-			{
-				Debug.Log("Load_gameData:" + Load_gameData[i]);
-			}
-		}
-	}
-	// Update is called once per frame
-	void Update()
-	{
-
-	}
+         }
+    }
+    /*reads files*/
+   public void read()
+    {
+        using (StreamReader sr = new StreamReader(filePath_cards))
+        {
+            Load_cards = sr.ReadLine().Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+        }
+        using(StreamReader sr = new StreamReader(filePath_gameData))
+        {
+            List<string> gameDataList = new List<string>(Load_gameData);
+            while (sr.Peek() >= 0)
+            {
+                gameDataList.Add(sr.ReadLine()); //adds one line at a time to datalist 
+            }
+            Load_gameData = gameDataList.ToArray(); //converts the list to an string array 
+            for (int i = 0; i < Load_gameData.GetLength(0); i++)
+            {
+                Debug.Log("Load_gameData:" + Load_gameData[i]);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
 
 

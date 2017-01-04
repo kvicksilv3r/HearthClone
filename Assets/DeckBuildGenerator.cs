d092a41ee205822a83173c1703b2a2dac577ff37
kvicksilv3r <douglas.ekehammar@gmail.com>
2017-01-04 13:07:22 +0100

    Deckbuilding scene and early scripts for it
    I broke stuff bit i fixed them so thats nice

---
+++ Assets/DeckBuildGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckBuildGenerator : MonoBehaviour {
+
+	public GameObject[] cards = new GameObject[8];
+	int generatorIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.G))
+		{
+			GenerateCards();
+			generatorIndex++;
+		}
+	}
+
+	void GenerateCards()
+	{
+		for(int i =0; i< cards.Length; i++)
+		{
+			cards[i].GetComponent<CardGenerator>().GenerateCard(i + (generatorIndex * 8) +1);
+		}
+	}
+}

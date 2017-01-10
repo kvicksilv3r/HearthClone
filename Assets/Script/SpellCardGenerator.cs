0f211ebdb4a953c46881dc030a4ad9e5d9e0a19d
kvicksilv3r <douglas.ekehammar@gmail.com>
2017-01-10 20:19:08 +0100

    Fixed spell generator and stuff

--- Assets/Script/SpellCardGenerator.cs
+++ Assets/Script/SpellCardGenerator.cs
@@ -5,46 +5,56 @@ using UnityEngine.UI;
 public class SpellCardGenerator : MonoBehaviour
 {
 
-    public string cardPictureName;
-    string pictureAssetName;
-    [SerializeField]
-    GameObject portrait;
-    [SerializeField]
-    GameObject manaTextObj;
-    [SerializeField]
-    GameObject cardTextObj;
-    [SerializeField]
-    GameObject cardNameTextObj;
-    [SerializeField]
-    GameObject gemObj;
-    [SerializeField]
-    GameObject gemHolderObj;
-    [SerializeField]
-    GameObject cardFaceObj;
-    GameObject card;
+	public string cardPictureName;
+	protected Spell spell;
+	protected GameManager gameManager;
+	string pictureAssetName;
+	[SerializeField]
+	GameObject portrait;
+	
+	[SerializeField]
+	GameObject manaTextObj;
+	[SerializeField]
+	GameObject cardTextObj;
+	[SerializeField]
+	GameObject cardNameTextObj;
+	[SerializeField]
+	GameObject portraitObj;
+	[SerializeField]
+	GameObject gemObj;
+	[SerializeField]
+	GameObject gemHolderObj;
+	[SerializeField]
+	GameObject cardFaceObj;
+	ParseFromJSON json;
+	CARDS c;
 
-    void Update()
+	void Start()
+	{
+		json = GameObject.Find("GameManager").GetComponent<ParseFromJSON>();
+	}
+
+    public void GenerateCard(CARDS card)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-          //  GenerateCard(0);
-        }
-    }
+		c = card;
 
-    public void GenerateCard(CARDS c)
-    {
-        pictureAssetName = "Assets/Cards/Textures/" + cardPictureName + ".png";
-        portrait.GetComponent<MeshRenderer>().material.mainTexture = (Texture2D)AssetDatabase.LoadAssetAtPath(pictureAssetName, typeof(Texture2D));
+		spell = GetComponent<Spell>();
+		GetComponent<CardClass>().Card = card;
 
-        manaTextObj.GetComponent<Text>().text = "10";
-        cardTextObj.GetComponent<Text>().text = "Deal 10 Damage.";
-        cardNameTextObj.GetComponent<Text>().text = "Pyroblast";
+		pictureAssetName = "Assets/Cards/Textures/" + c.picture_name + ".png";
+		portrait.GetComponent<MeshRenderer>().material.mainTexture = (Texture2D)AssetDatabase.LoadAssetAtPath(pictureAssetName, typeof(Texture2D));
+		
+		manaTextObj.GetComponent<Text>().text = c.mana.ToString();
+		cardTextObj.GetComponent<Text>().text = c.description;
+		cardNameTextObj.GetComponent<Text>().text = c.card_name;
 
-        gemHolderObj.SetActive(true);
+		gemHolderObj.SetActive(true);
         gemObj.SetActive(true);
 
-        cardFaceObj.GetComponent<MeshRenderer>().material.mainTexture = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Cards/Textures/Cardfronts/card_spell_neutral.png", typeof(Texture2D));
-        transform.GetComponent<CardClass>().CardName = cardNameTextObj.GetComponent<Text>().text;
-    }
+		gemObj.GetComponent<MeshRenderer>().material.mainTexture = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Cards/Textures/Gems/gem_" + c.rarity + ".png", typeof(Texture2D));
+
+		cardFaceObj.GetComponent<MeshRenderer>().material.mainTexture = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Cards/Textures/Cardfronts/card_spell_" + c.class_name + ".png", typeof(Texture2D));
+		transform.GetComponent<CardClass>().CardName = cardNameTextObj.GetComponent<Text>().text;
+	}
 
 }

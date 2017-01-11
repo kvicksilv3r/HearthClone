23a505e5af85c75de067f04827472ac797898af0
kvicksilv3r <douglas.ekehammar@gmail.com>
2017-01-11 16:34:20 +0100

    Aibattle x to the D

--- Assets/Script/AIBattle.cs
+++ Assets/Script/AIBattle.cs
@@ -4,187 +4,183 @@ using UnityEngine;
 
 public class AIBattle : MonoBehaviour
 {
-    //GameObject[] targets;
+	//GameObject[] targets;
 
-    List<GameObject> TargetList;
-    GameObject attackTarget;
-    Creature aiCreature;
-    Creature playerCreature;
-    GameManager gameManager;
+	List<GameObject> TargetList;
+	Creature attackTarget;
+	Creature aiCreature;
+	Creature playerCreature;
+	GameManager gameManager;
 	List<Creature> killabeTaunt = new List<Creature>();
 	List<Creature> tauntCreatures = new List<Creature>();
 	List<Creature> killableCreatures = new List<Creature>();
 	List<Creature> rest = new List<Creature>();
 	float waitTime = 2;
+	bool goFace = false;
 
-    void Start()
-    {
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        TargetList = new List<GameObject>(GameObject.Find("Player Playfield").transform.childCount);
-    }
+	void Start()
+	{
+		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		TargetList = new List<GameObject>(GameObject.Find("Player Playfield").transform.childCount);
+	}
 
-    public void AIBattlePhase()
-    {
-        StartCoroutine("BattlePhase");
-    }
+	public void AIBattlePhase()
+	{
+		StartCoroutine("BattlePhase");
+	}
 
-    IEnumerator BattlePhase()
-    {
-        TargetList.Clear();
+	IEnumerator BattlePhase()
+	{
+		TargetList.Clear();
 
-        foreach (Transform target in GameObject.Find("Player Playfield").transform)
-        {
-            TargetList.Add(target.gameObject);
-        }
+		goFace = CanKillHero();
 
-        foreach (Transform child in transform)
-        {
-            aiCreature = child.GetChild(0).GetComponent<Creature>();
+		foreach (Transform target in GameObject.Find("Player Playfield").transform)
+		{
+			TargetList.Add(target.gameObject);
+		}
 
-            if (CheckCreature())
-            {
-                if (aiCreature.CurrentAttacks >= 1 && aiCreature.CanAttack && attackTarget != null)
-                {
-                    if (aiCreature.CurrentAttacks > 1 && playerCreature.Health > 0)
-                    {
-                        attackTarget.transform.GetChild(0).GetComponent<Creature>().TakeDamage(aiCreature.Strength);
-                        aiCreature.TakeDamage(playerCreature.Strength);
+		foreach (Creature creature in transform.GetComponentsInChildren<Creature>())
+		{
+			aiCreature = creature;
+			while (creature.CurrentAttacks >= 1 && aiCreature.CanAttack)
+			{
+				if (CheckCreature())
+				{
+					if (attackTarget != null)
+					{
+						yield return new WaitForSeconds(waitTime);
+						attackTarget.TakeDamage(aiCreature.Strength);
+						aiCreature.TakeDamage(attackTarget.Strength);
+						aiCreature.CurrentAttacks--;
+						
+					}
+					else
+					{
+						yield return new WaitForSeconds(waitTime);
+						AttackHero(aiCreature.Strength);
+					}
+				}
+				else
+				{
+					yield return new WaitForSeconds(waitTime);
+					AttackHero(aiCreature.Strength);
+				}
+			}
+		}
 
-                        Debug.Log("First target hit");
+		yield return new WaitForSeconds(waitTime);
 
-                        aiCreature.CurrentAttacks--;
+		gameManager.NextRound(false);
 
-                        if (playerCreature.Health < 1)
-                        {
-                            Debug.Log("Target killed on first attack");
+		yield return null;
+	}
 
-                            attackTarget = null;
-                        }
-                    }
+	void AttackHero(int dmg)
+	{
+		gameManager.HeroDamage(0, dmg);
+		aiCreature.CurrentAttacks--;
+	}
 
-                    if (attackTarget == null)
-                    {
-                        Debug.Log("New target");
+	bool CanKillHero()
+	{
+		int dmg = 0;
 
-                        //recheck here yo
+		foreach (Creature cr in GameObject.Find("Player Playfield").transform.GetComponentsInChildren<Creature>())
+		{
+			if (cr.HasTaunt)
+			{
+				return false;
+			}
+		}
 
-                        if (attackTarget == null)
-                        {
-                            Debug.Log("no targets");
-                            break;
-                        }
-                    }
+		foreach (Creature cr in transform.GetComponentsInChildren<Creature>())
+		{
+			if (cr.CanAttack && cr.CurrentAttacks > 0)
+				dmg += cr.Strength * cr.CurrentAttacks;
+		}
 
-                    yield return new WaitForSeconds(waitTime);
+		if (dmg > gameManager.Players()[0].health)
+		{
+			return true;
+		}
 
-                    if (aiCreature.Health > 0 && playerCreature.Health > 0)
-                    {
-                        playerCreature.TakeDamage(aiCreature.Strength);
-                        aiCreature.TakeDamage(playerCreature.Strength);
+		else
+		{
+			return false;
+		}
+	}
 
-                        Debug.Log("Second Attack");
-
-                        aiCreature.CurrentAttacks--;
-
-                        yield return new WaitForSeconds(waitTime);
-                    }
-                }
-            }
-            else if (aiCreature.CanAttack && aiCreature.CurrentAttacks > 1)
-            {
-                gameManager.HeroDamage(0, aiCreature.Strength);
-                yield return new WaitForSeconds(waitTime);
-
-                aiCreature.CurrentAttacks--;
-            }
-            else if (aiCreature.CanAttack && aiCreature.CurrentAttacks > 0)
-            {
-                gameManager.HeroDamage(0, aiCreature.Strength);
-                yield return new WaitForSeconds(waitTime);
-
-                aiCreature.CurrentAttacks--;
-            }
-        }
-
-        yield return new WaitForSeconds(waitTime);
-
-        gameManager.NextRound(false);
-
-        yield return null;
-    }
-
-    public bool CheckCreature()
-    {
-        bool returnBool = false;
-
+	public bool CheckCreature()
+	{
 		killabeTaunt.Clear();
 		tauntCreatures.Clear();
 		killableCreatures.Clear();
 		rest.Clear();
 
-        if (GameObject.Find("Player Playfield").transform.childCount <= 0)
-        {
-            return false;
-        }
+		if (GameObject.Find("Player Playfield").transform.childCount <= 0)
+		{
+			return false;
+		}
 
-        foreach (Transform target in GameObject.Find("Player Playfield").transform)
-        {
-            playerCreature = target.transform.GetChild(0).GetComponent<Creature>();
+		foreach (Creature creature in GameObject.Find("Player Playfield").transform.GetComponentsInChildren<Creature>())
+		{
+			playerCreature = creature;
 
-            if (aiCreature.Strength >= playerCreature.Health && playerCreature.GetComponent<Creature>())
-            {
+			if (aiCreature.Strength >= playerCreature.Health && playerCreature.GetComponent<Creature>())
+			{
 				killabeTaunt.Add(playerCreature);
 				continue;
-            }
-            else if (playerCreature.HasTaunt)
-            {
+			}
+			else if (playerCreature.HasTaunt)
+			{
 				tauntCreatures.Add(playerCreature);
 				continue;
-            }
-            else if (aiCreature.Strength >= playerCreature.Health && aiCreature.Health > playerCreature.Strength)
-            {
+			}
+			else if (aiCreature.Strength >= playerCreature.Health && aiCreature.Health > playerCreature.Strength)
+			{
 				killableCreatures.Add(playerCreature);
 				continue;
-            }
-            else if(aiCreature.CanAttack && aiCreature.CurrentAttacks > 0)
-            {
+			}
+			else if (aiCreature.CanAttack && aiCreature.CurrentAttacks > 0)
+			{
 				rest.Add(playerCreature);
 				continue;
-            }
-        }
-        
-		if(killabeTaunt.Count > 0)
+			}
+		}
+
+		if (killabeTaunt.Count > 0)
 		{
-			attackTarget = killabeTaunt[Random.Range(0, killabeTaunt.Count)].gameObject;
+			attackTarget = killabeTaunt[Random.Range(0, killabeTaunt.Count)];
 			return true;
 		}
-		else if(tauntCreatures.Count > 0)
+		else if (tauntCreatures.Count > 0)
 		{
-			attackTarget = tauntCreatures[Random.Range(0, tauntCreatures.Count)].gameObject;
+			attackTarget = tauntCreatures[Random.Range(0, tauntCreatures.Count)];
 			return true;
 		}
 		else if (killableCreatures.Count > 0)
 		{
-			attackTarget = killableCreatures[Random.Range(0, killableCreatures.Count)].gameObject;
+			attackTarget = killableCreatures[Random.Range(0, killableCreatures.Count)];
 			return true;
 		}
-		else if(rest.Count > 0)
+		else if (rest.Count > 0)
 		{
-			if(Random.Range(0,3) != 0)
+			if (Random.Range(0, 3) != 0)
 			{
-				attackTarget = rest[Random.Range(0, rest.Count)].gameObject;
-				returnBool = true;
+				attackTarget = rest[Random.Range(0, rest.Count)];
+				return true;
 			}
 			else
 			{
-				returnBool = false;
+				return false;
 			}
 		}
 		else
 		{
-			returnBool = false;
+			return false;
 		}
 
-		return returnBool;
-    }
+		return false;
+	}
 }
